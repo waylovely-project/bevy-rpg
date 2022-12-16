06f65d95742e8494d1cd855bfbc542deaf0293a4
Fiana Fortressia <fortressnordlys@outlook.com>
2022-12-16 19:00:38 +0700

    Drop UITree usage in favor of Name
    Signed-off-by: Fiana Fortressia <fortressnordlys@outlook.com>

--- src/ui.rs
+++ src/ui.rs
@@ -3,10 +3,8 @@ use std::thread::sleep;
 use std::time::Duration;
 
 use crate::characters::{text_style, CharacterName};
-use crate::dialog::DialogIncomingEvent;
 use crate::Dialog;
-use bevy::ui::widget::ImageMode;
-use bevy::{prelude::*, text};
+use bevy::{prelude::*};
 
 #[derive(Resource, Default)]
 pub struct DialogIter {
@@ -24,17 +22,11 @@ impl Iterator for DialogIter {
         Some(self.dialogs[current].clone())
     }
 }
-#[derive(Resource)]
-pub struct UITree {
-    pub character_box: Entity,
-    pub text_box: Entity,
-}
+
 pub fn ui(mut commands: Commands, server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    let mut char_box = None;
-    let mut text_box = None;
     commands
-        .spawn((NodeBundle {
+        .spawn((ButtonBundle {
             background_color: BackgroundColor(Color::PINK),
             style: Style {
                 flex_direction: FlexDirection::Column,
@@ -59,9 +51,9 @@ pub fn ui(mut commands: Commands, server: Res<AssetServer>) {
             ..default()
         }, Name::new("dialog-box")))
         .with_children(|parent| {
-            char_box = Some(parent.spawn(TextBundle { ..default() }).id());
+            parent.spawn((TextBundle { ..default() }, Name::new("character-box")));
 
-            text_box = Some(parent.spawn(TextBundle { ..default() }).id());
+            parent.spawn((TextBundle { ..default() }, Name::new("text-box") ));
         });
     commands
         .spawn((NodeBundle {
@@ -121,40 +113,43 @@ pub fn ui(mut commands: Commands, server: Res<AssetServer>) {
 
             //
         });
-    commands.insert_resource(UITree {
-        character_box: char_box.unwrap(),
-        text_box: text_box.unwrap(),
-    })
+ 
 }
 
 pub fn update_dialog(
-    mut query: Query<(Entity, &mut Text)>,
-    tree: Res<UITree>,
+    mut query: Query<(&Name, &mut Text)>,
+  
     mut dialog_iter: ResMut<DialogIter>,
 ) {
     if dialog_iter.dialogs.len() == 0 { return; }
     let dialog = &dialog_iter.dialogs[dialog_iter.current];
+    {
+    let (_, mut char_text) = query.iter_mut().find(|( name, _)| name.as_str() == "character_box").unwrap();
+   
+    match dialog {
+        Dialog::Text(dialog) => {
+            *char_text = dialog
+                .charname()
+                .unwrap_or_else(|| Text::from_section("Unknown", Default::default()));
 
-    for (id, mut text) in query.iter_mut() {
-        if id == tree.character_box {
-            match dialog {
-                crate::Dialog::Text(dialog) => {
-                    *text = dialog
-                        .charname()
-                        .unwrap_or_else(|| Text::from_section("Unknown", Default::default()))
-                }
-                crate::Dialog::Choose(choose) => {}
-            };
-        } else if id == tree.text_box {
-            match dialog {
-                crate::Dialog::Text(dialog) => {
-                    *text = index_text(&dialog.text, dialog_iter.current_char_step)
-                    //*text = dialog.text.clone()
-                }
-                crate::Dialog::Choose(choose) => {}
-            };
+         
         }
+        crate::Dialog::Choose(choose) => {
+            warn!("ChooseDialog support is not implemented yet: {choose:#?}");
+        }
+    };
     }
+     let (_, mut text_text) = query.iter_mut().find(|( name, _)| name.as_str() == "text-box").unwrap();
+    
+    match dialog {
+        Dialog::Text(dialog) => {
+        *text_text = index_text(&dialog.text, dialog_iter.current_char_step);
+        },
+        _ => {}
+    }
+   
+        
+    
 
     dialog_iter.current_char_step += 1;
 
